a7ad513fd63c048a15351a8b5ab8c9ecd3e3f2e8
Aryan8400 <aj628980@gmail.com>
2024-10-23 09:40:27 +0545

    making responsive business section

--- src/components/BusinessSection.jsx
+++ src/components/BusinessSection.jsx
@@ -7,7 +7,7 @@ const BusinessSection = () => {
         
         {/* Text Section */}
         <div className="lg:w-1/2 text-center lg:text-left lg:pr-12 mt-8 lg:mt-0">
-          <h2 className="text-2xl sm:text-3xl md:text-4xl font-semibold text-gray-900 mb-6">
+          <h2 className="text-xl sm:text-2xl md:text-3xl lg:text-4xl font-semibold text-gray-900 mb-6">
             With Vyapar, a Businessman can effortlessly
           </h2>
           <ul className="list-disc text-gray-600 text-sm sm:text-base md:text-lg space-y-3">
